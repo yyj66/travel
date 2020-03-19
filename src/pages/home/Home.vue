04ee3ca7c004ddee108176b71f907ef24b5c013d
yyj66 <yyj6699@outlook.com>
2020-03-19 20:06:41 +0800

    添加首页的header组件

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <home-header></home-header>
-    </div>
+<div>
+    <home-header></home-header>
+</div>
 </template>
 <script>
 import HomeHeader from './components/HomeHeader'

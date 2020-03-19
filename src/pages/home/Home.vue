0bed74f12b9fbf391d4e817134cf6e5633954498
yyj66 <yyj6699@outlook.com>
2020-03-19 19:49:38 +0800

    初始化项目结构

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <home-header></home-header>
+    </div>
+</template>
+<script>
+import HomeHeader from './components/HomeHeader'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/pages/home/components/HomeHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>header</div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style scoped>
+
+</style>

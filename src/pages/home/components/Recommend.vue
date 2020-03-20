b710f1efd886a598b7b19c738fdaa9dd02f82097
yyj66 <yyj6699@outlook.com>
2020-03-20 16:04:34 +0800

    提交首页列表组件

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <div class="title">热门推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img"
+                :src="item.imgURL"/>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '001',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/201405/14/2324f88e83f7b8675dd9e74a3c2f2fa5.jpg_200x200_e8cf9882.jpg',
+          title: '同里古镇',
+          desc: '真的是特别的漂亮,真的是特别的漂亮,真的是特别的漂亮,重要的事情说三遍'
+        },
+        {
+          id: '002',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/201405/14/2324f88e83f7b8675dd9e74a3c2f2fa5.jpg_200x200_e8cf9882.jpg',
+          title: '同里古镇',
+          desc: '真的是特别的漂亮,真的是特别的漂亮,真的是特别的漂亮,重要的事情说三遍'
+        },
+        {
+          id: '003',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/201405/14/2324f88e83f7b8675dd9e74a3c2f2fa5.jpg_200x200_e8cf9882.jpg',
+          title: '同里古镇',
+          desc: '真的是特别的漂亮,真的是特别的漂亮,真的是特别的漂亮,重要的事情说三遍'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+  .title
+    margin-top :.2rem
+    line-height :.8rem
+    text-indent :.2rem
+    background :#eee
+  .item
+    overflow :hidden
+    display :flex
+    height :1.9rem
+    .item-img
+      width :1.7rem
+      height :1.7rem
+      padding:.1rem
+    .item-info
+      flex:1
+      min-width :0
+      padding :.1rem
+      .item-title
+        line-height :.54rem
+        font-size :.32rem
+        ellipsis()
+      .item-desc
+        line-height :.4rem
+        color :#ccc
+        ellipsis()
+      .item-button
+        line-height :.44rem
+        margin-top :.16rem
+        background :#ff9300
+        padding :0 .1rem
+        border-radius:.06rem
+
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+               <div class="item-img-wrapper">
+                  <img class="item-img" :src="item.imgURL"/>
+               </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '001',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1505/8a/d412db9fcd44dd.jpg_r_640x214_86f303a2.jpg',
+          title: '苏州园林',
+          desc: '真的是特别的漂亮,真的是特别的漂亮,真的是特别的漂亮,重要的事情说三遍'
+        },
+        {
+          id: '002',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1505/8a/d412db9fcd44dd.jpg_r_640x214_86f303a2.jpg',
+          title: '苏州园林',
+          desc: '真的是特别的漂亮,真的是特别的漂亮,真的是特别的漂亮,重要的事情说三遍'
+        },
+        {
+          id: '003',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1505/8a/d412db9fcd44dd.jpg_r_640x214_86f303a2.jpg',
+          title: '苏州园林',
+          desc: '真的是特别的漂亮,真的是特别的漂亮,真的是特别的漂亮,重要的事情说三遍'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+  .title
+    margin-top :.2rem
+    line-height :.8rem
+    text-indent :.2rem
+    background :#eee
+  .item-img-wrapper
+    overflow :hidden
+    height :0
+    padding-bottom :33.9%
+    .item-img
+      width :100%
+  .item-info
+    padding :.1rem
+    .item-title
+      line-height :.54rem
+      font-size :.32rem
+      ellipsis()
+    .item-desc
+      line-height :.4rem
+      color :#ccc
+      ellipsis()
+
+</style>
